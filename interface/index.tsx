a74eec96cf5abbd2b5f96fa3cc327c49d8d4b194
ayman elfadl <aymanelfadl16@gmail.com>
2025-05-14 16:27:26 +0100

    ok

---
+++ interface/index.tsx
@@ -0,0 +1,13 @@
+
+interface CardProps {
+    title: string;
+    description: string;
+    imageUrl: string;
+    price: number;
+}
+
+
+interface ButtonProps {
+    title: string;
+    onClick: () => void;
+}
